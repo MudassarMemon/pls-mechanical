b90480afe03a3d3c355b040e29eb6d976ec43eac
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2023-12-11 18:43:12 -0500

    Build interactive cooling and heating diagram

---
+++ js/config-heating-interactive.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //interactive heat diagram
+
+    var interactiveHover = document.querySelectorAll('.interactiveHover');
+    var interactiveOverlay = document.querySelectorAll('.defaultimg');
+    let defaultBackground = interactiveOverlay[2].getAttribute('style')
+    console.log(defaultBackground.includes("cooling"))
+    
+    interactiveHover.forEach(function(area){
+        
+        area.addEventListener("mouseover", function(){
+            let overlay = this.getAttribute('id')
+
+            interactiveOverlay[2].style.backgroundImage = "url(\"images/" + overlay + ".png\")"
+        })
+
+        area.addEventListener("mouseleave", function(){
+            if (defaultBackground.includes("heating")){
+                interactiveOverlay[2].style.backgroundImage = "url(\"images/heating-interactive-bg.png\")"
+            } else{
+                interactiveOverlay[2].style.backgroundImage = "url(\"images/cooling-interactive-bg.png\")"
+            }
+        })
+    })
+ 
+})
