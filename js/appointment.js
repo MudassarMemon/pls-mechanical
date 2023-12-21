422dcb52e0ca193c989dcdaf6edba040739637fd
Mudassar Memon <128174228+MudassarMemon@users.noreply.github.com>
2023-12-20 22:56:43 -0500

    Style appointment form

--- js/appointment.js
+++ js/appointment.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("two-step-form");
     const freeAssessment = document.getElementById("free-assessment");
     const stepOneValidate = document.querySelector(".step_one_validate");
+    const apptStepTwo = document.querySelector(".appt-step-2");
+    const apptStepOneValues = document.querySelector(".appt-step-1-values");
+
 
 
     nextButton.addEventListener("click", function () {
@@ -20,11 +23,15 @@ document.addEventListener("DOMContentLoaded", function () {
         // If all validations pass, proceed to Step 2
         form.querySelector(".form-step-one").style.display = "none";
         form.querySelector(".form-step-two").style.display = "block";
+        apptStepTwo.style.borderColor = "#00D1FF";
+        apptStepOneValues.innerHTML = service
+        apptStepOneValues.style.display = "block"
     });
 
     backButton.addEventListener("click", function () {
         // If all validations pass, proceed to Step 2
         form.querySelector(".form-step-one").style.display = "block";
         form.querySelector(".form-step-two").style.display = "none";
+        apptStepTwo.style.borderColor = "#262D33";
     });
 });
